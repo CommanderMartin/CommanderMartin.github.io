30a3d34bb5af9abcbec150feeda3592988833c23
CommanderMartin <41338539+CommanderMartin@users.noreply.github.com>
2018-07-21 14:51:24 -0400

    Delete header.php

--- assets/CapstoneSiteDemo/includes/header.php
+++
@@ -1,6 +0,0 @@
-<header>
-<div class="homePage">
-	<a href="index.php"><img src="images/Header.png" alt="Middle Georgia Justice" id="mainLogo" /></a>
-	<img src="images/newLogo.png" alt="Logo" id="sideLogo" />
-</div>
-</header>
